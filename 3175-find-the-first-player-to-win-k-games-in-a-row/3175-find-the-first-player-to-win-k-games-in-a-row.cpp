0d694a1a0bb87800db07d6df45f0f3add7a0dcb5
navya1129 <147473545+navya1129@users.noreply.github.com>
2026-07-23 13:59:13 +0530

    Time: 3403 ms (5.02%), Space: 147.4 MB (14.16%) - LeetHub

---
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findWinningPlayer(vector<int>& skills, int k) {
+        deque<int> dq;
+        int n = skills.size();
+        auto maxi = max_element(skills.begin(),skills.end());
+        int max_idx = distance(skills.begin(), maxi);
+        if(k >= n) return distance(skills.begin(),max_element(skills.begin(),skills.end()));
+        for(int i = n-1;i >= 0;i--){
+            dq.push_front(i);
+        }
+        map<int,int> m;
+        while(true){
+            int one = dq.front();
+            dq.pop_front();
+            int two = dq.front();
+            dq.pop_front();
+            int cnt = 0;
+            cout << "values" << "\n";
+            cout << one << " " << two << "\n";
+            if(skills[one] > skills[two]){
+                while(skills[one] > skills[two]){
+                    dq.push_back(two);
+                    two = dq.front();
+                    dq.pop_front();
+                    m[one]++;
+                    cout << one << " " << m[one] << "\n";
+                    if(m[one] == k) return one;
+                }
+                // dq.push_back(one);
+                // dq.push_front(two);
+            }
+            else{
+                while(skills[one] < skills[two]){
+                    dq.push_back(one);
+                    one = dq.front();
+                    dq.pop_front();
+                    m[two]++;
+                    cout << two << " " << m[two] << "\n";
+                    if(m[two] == k) return two;
+                }
+                // dq.push_back(two);
+                // dq.push_front(one);
+            }
+            dq.push_front(one);
+            dq.push_front(two);
+        }
+        return -1;
+    }
+};
