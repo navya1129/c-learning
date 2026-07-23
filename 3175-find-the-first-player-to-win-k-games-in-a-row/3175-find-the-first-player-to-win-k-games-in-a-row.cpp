c8500e06c2f1e76920c9f6bde2b875908da9b996
navya1129 <147473545+navya1129@users.noreply.github.com>
2026-07-23 14:00:09 +0530

    Time: 99 ms (10.96%), Space: 147.5 MB (14.16%) - LeetHub

--- 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -3,19 +3,14 @@ public:
     int findWinningPlayer(vector<int>& skills, int k) {
         deque<int> dq;
         int n = skills.size();
-        auto maxi = max_element(skills.begin(),skills.end());
-        int max_idx = distance(skills.begin(), maxi);
         if(k >= n) return distance(skills.begin(),max_element(skills.begin(),skills.end()));
-        for(int i = n-1;i >= 0;i--){
-            dq.push_front(i);
-        }
+        for(int i = n-1;i >= 0;i--) dq.push_front(i);
         map<int,int> m;
         while(true){
             int one = dq.front();
             dq.pop_front();
             int two = dq.front();
             dq.pop_front();
-            int cnt = 0;
             if(skills[one] > skills[two]){
                 while(skills[one] > skills[two]){
                     dq.push_back(two);
@@ -31,7 +26,6 @@ public:
                     one = dq.front();
                     dq.pop_front();
                     m[two]++;
-                    cout << two << " " << m[two] << "\n";
                     if(m[two] == k) return two;
                 }
             }
